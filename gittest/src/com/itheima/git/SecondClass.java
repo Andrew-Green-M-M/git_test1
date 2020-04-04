1811ac1c45d098c1617146c28d87c13750be8815
zouchangan <zouchangan@163.com>
2020-04-04 17:56:21 +0800

    git分支创建

--- gittest/src/com/itheima/git/SecondClass.java
+++ gittest/src/com/itheima/git/SecondClass.java
@@ -5,5 +5,8 @@ import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("git 分支 dev");
+        System.out.println("git 分支 dev");
+        System.out.println("git 分支 dev");
+        System.out.println("git 分支 dev");
     }
 }
