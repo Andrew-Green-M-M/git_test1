fc7a7b0550b6ba61f6ed8c2d8bdb14b2d4f41470
zouchangan <zouchangan@163.com>
2020-04-04 17:08:36 +0800

    git创建

---
+++ gittest/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git 测试");
+        System.out.println("git 测试");
+        System.out.println("git 测试");
+        System.out.println("git 测试");
+    }
+    public void test1(){
+        Integer n=10;
+        n+=10;
+    }
+}
